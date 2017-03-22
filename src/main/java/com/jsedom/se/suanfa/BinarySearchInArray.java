df5e2a82ceef6b8d025f224b9d3fb9da2415762a
wangx <wxb1989.wxb@163.com>
2017-03-22 13:40:37 +0800

    基础算法

--- src/main/java/com/jsedom/se/suanfa/BinarySearchInArray.java
+++ src/main/java/com/jsedom/se/suanfa/BinarySearchInArray.java
@@ -103,6 +103,11 @@ public class BinarySearchInArray {
     public static void main(String[] args) {
         printSample();
         System.out.println(getValuefromMatrix(sample, 4, 4, 8));
+        int a = 5; int b = 10;
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
 
+        System.out.println(a+ "   " + b);
     }
 }
