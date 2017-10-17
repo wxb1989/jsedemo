be68e86229664b63549089e6db6751c9c992e75e
wangx <wxb1989.wxb@163.com>
2017-10-17 15:15:43 +0800

    基础算法

--- src/main/java/com/jsedom/se/dataalgorithms/mylist/MyList.java
+++ src/main/java/com/jsedom/se/dataalgorithms/mylist/MyList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * 自己实现普通的List
+ * 自己实现普通的List（有点问题）
  */
 public class MyList<Key> {
 
@@ -12,6 +12,7 @@ public class MyList<Key> {
     private Object [] elementData;//存放数据的数组
 
     private int INIT_SIZE=10;
+    private int size;
 
     public MyList() {
         this.elementData = new Object [INIT_SIZE];
@@ -26,31 +27,60 @@ public class MyList<Key> {
         int currentIdx = elementData.length;
         if(currentIdx ==0){
             elementData[0]=key;
+            size=elementData.length;
             return true;
         }
         if(elementData.length==INIT_SIZE){
             elementData = Arrays.copyOf(elementData,INIT_SIZE*2);
             elementData[currentIdx+1]=key;
+            size=elementData.length;
             return true;
         }
         return false;
     }
 
     public boolean remove(int index){
-        if(index >= elementData.length){
+        if(index > size){
             throw new IndexOutOfBoundsException();
         }
 
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elementData, index + 1, elementData, index,
+                    numMoved);
+        }
+        elementData[--size] = null;
         return false;
     }
 
 
     public int size() {
-        return elementData.length;
+        return size;
+    }
+    public Key get(int index){
+        return (Key) elementData[index];
     }
 
     public boolean isEmpty() {
         return elementData.length == 0?true:false;
     }
 
+
+    public static void main(String[] args) {
+        MyList<String>myList = new MyList<String>(20);
+        myList.add("aaaa");
+        myList.add("bbbb");
+        myList.add("cccc");
+        myList.add("dddd");
+        myList.add("eeee");
+        myList.add("ffff");
+        myList.add("gggg");
+        myList.add("hhhh");
+
+        for (int i = 0; i < myList.size(); i++) {
+            String a  =myList.get(i);
+            System.out.println(a);
+        }
+
+    }
 }
