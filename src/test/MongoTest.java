caef9ca3d109abf86cc410f732b9e1e5308a3f3c
wangx <wxb1989.wxb@163.com>
2017-12-09 12:19:50 +0800

    基础算法

--- src/test/MongoTest.java
+++ src/test/MongoTest.java
@@ -33,4 +33,63 @@ public class MongoTest {
             System.out.println(doc.toJson());
         }
     }
+
+    @Test
+    public void testFindBy(){
+        Document filter = new Document();
+        filter.append("name", "li si");
+        List<Document> results = MongoDb.findBy(filter);
+        for(Document doc : results){
+            System.out.println(doc.toJson());
+        }
+    }
+
+    @Test
+    public void testUpdateOne(){
+        Document filter = new Document();
+        filter.append("gender", "male");
+
+        //注意update文档里要包含"$set"字段
+        Document update = new Document();
+        update.append("$set", new Document("gender", "female"));
+        UpdateResult result = MongoDb.updateOne(filter, update);
+        System.out.println("matched count = " + result.getMatchedCount());
+    }
+
+    @Test
+    public void testUpdateMany(){
+        Document filter = new Document();
+        filter.append("gender", "female");
+
+        //注意update文档里要包含"$set"字段
+        Document update = new Document();
+        update.append("$set", new Document("gender", "male"));
+        UpdateResult result = MongoDb.updateMany(filter, update);
+        System.out.println("matched count = " + result.getMatchedCount());
+    }
+
+    @Test
+    public void testReplace(){
+        Document filter = new Document();
+        filter.append("name", "zhang");
+
+        //注意：更新文档时，不需要使用"$set"
+        Document replacement = new Document();
+        replacement.append("value", 123);
+        MongoDb.replace(filter, replacement);
+    }
+
+    @Test
+    public void testDeleteOne(){
+        Document filter = new Document();
+        filter.append("name", "li");
+        MongoDb.deleteOne(filter);
+    }
+
+    @Test
+    public void testDeleteMany(){
+        Document filter = new Document();
+        filter.append("gender", "male");
+        MongoDb.deleteMany(filter);
+    }
 }
